0e0e617c27c09962d795a3d7003331e4285b081a
Vadim Ivanov <vadim@givola.com>
2020-02-17 22:55:08 +0300

    update package.json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,67 @@
-const webpack = require('webpack');
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
     filename: 'index.js',
-    library: 'vue-stripe-elements',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  resolve: {
-    extensions: ['.js', '.vue']
+    library:'vue-google-pay',
+    libraryTarget: 'umd'
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      include: __dirname,
-      exclude: /node_modules/
-    }, {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    }]
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: {
+    vue: 'vue'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: false,
+        parallel: 4,
+        uglifyOptions: {
+          warnings: false
+        },
+      })
+    ]
+  },
+  performance: {
+    hints: false
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      sourceMap: false,
-      mangle: true,
-      compress: {
-          warnings: false
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
       }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
     })
   ]
-};
+}
